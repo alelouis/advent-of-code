330e376d2f12136c6491391b7efab0d324aa0cc1
Alexis Louis <wimacod@gmail.com>
2020-12-13 12:37:59 +0100

    day 13

--- 2020/day-13/main.py
+++ 2020/day-13/main.py
@@ -8,12 +8,13 @@ d_times = [i*ceil(timestep/i)-timestep for i in ids]
 min_t, bus_id = min(d_times), int(ids[d_times.index(min(d_times))])
 print(f'part-1 answer: {bus_id*min_t}')
 
-M = reduce(mul, ids, 1)
+# https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+m = reduce(mul, ids, 1)
 ids, x = lines[1][:-1].split(','), 0
 for i in range(len(ids)):
     if ids[i] != 'x':
         a_i = int(ids[i]) - i
-        M_i = int(M/int(ids[i]))
-        y_i = pow(M_i, -1, int(ids[i]))
-        x += a_i * M_i * y_i
-print(f'part-2 answer: {x%M}')
+        m_i = int(m/int(ids[i]))
+        y_i = pow(m_i, -1, int(ids[i]))
+        x += a_i * m_i * y_i
+print(f'part-2 answer: {x%m}')
