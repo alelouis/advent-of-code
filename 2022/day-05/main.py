954609bdc2143cceedb9ba32fc02af0935799df1
alelouis <me@alelouis.eu>
2022-12-05 19:05:57 +0100

    day-05 2022

--- 2022/day-05/main.py
+++ 2022/day-05/main.py
@@ -7,8 +7,7 @@ def solve(part, stacks, instructions):
         stacks[to-1] += [stacks[fro-1].pop() for _ in range(moves)][::part]
     return ''.join([stacks[i].pop() for i in stacks])
 
-rows, cols = 8, 9
-lines = open('input').readlines()
+rows, cols, lines = open('input').readlines(), 8, 9
 crates = [[l[2*i+1] for i in range(2*cols-1)] for l in lines[:rows]]
 stacks = {k//2: [crates[j][k] for j in range(rows) if crates[j][k].isalpha()][::-1] for k in range(0, cols*2, 2)}
 
