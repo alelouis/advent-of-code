9ca5afee27f2aadfdc8d74ca197356bc5cc30c90
alelouis <me@alelouis.eu>
2022-12-20 10:47:14 +0100

    day-20 2022

--- 2022/day-20/main.py
+++ 2022/day-20/main.py
@@ -1,19 +1,17 @@
 from copy import copy
 
-def mix(numbers, indices):
-    for i in range(len(numbers)):
-        if indices.index(i)+numbers[i] == 0: 
-            indices.insert(len(numbers)-1, indices.pop(indices.index(i)))
-        else: 
-            indices.insert((indices.index(i)+numbers[i])%(len(numbers)-1), indices.pop(indices.index(i)))
-    return indices
+def mix(num, idx):
+    for i in range(len(num)):
+        if idx.index(i)+num[i] == 0: idx.insert(len(num)-1, idx.pop(idx.index(i)))
+        else: idx.insert((idx.index(i)+num[i])%(len(num)-1), idx.pop(idx.index(i)))
+    return idx
 
-numbers = list(map(int, open("input").read().splitlines()))
+num = list(map(int, open("input").read().splitlines()))
 solve = lambda d, n : sum(d[g] for g in list((d.index(0) + j)%len(n) for j in (1000, 2000, 3000)))
-initial, indices = [o * 811589153 for o in numbers], list(range(len(numbers)))
+init, idx = [o * 811589153 for o in num], list(range(len(num)))
 for i in range(10):
-    indices = mix(copy(initial), copy(indices))
-    decrypted = [initial[j] for j in indices]
+    idx = mix(copy(init), copy(idx))
+    decrypted = [init[j] for j in idx]
 
-print(f"part-1 answer: {solve([numbers[j] for j in mix(copy(numbers), list(range(len(numbers))))], numbers)}")
-print(f"part-2 answer: {solve(decrypted, numbers)}")
+print(f"part-1 answer: {solve([num[j] for j in mix(copy(num), list(range(len(num))))], num)}")
+print(f"part-2 answer: {solve(decrypted, num)}")
