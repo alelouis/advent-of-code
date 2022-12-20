56317d94fb3d47926c2a8e8150e625c74991df62
alelouis <me@alelouis.eu>
2022-12-20 13:32:26 +0100

    day-20 2022

--- 2022/day-20/main.py
+++ 2022/day-20/main.py
@@ -9,9 +9,6 @@ def mix(num, idx):
 num = list(map(int, open("input").read().splitlines()))
 solve = lambda d, n : sum(d[g] for g in list((d.index(0) + j)%len(n) for j in (1000, 2000, 3000)))
 init, idx = [o * 811589153 for o in num], list(range(len(num)))
-for i in range(10):
-    idx = mix(copy(init), copy(idx))
-    decrypted = [init[j] for j in idx]
-
+for i in range(10): idx = mix(copy(init), copy(idx))
 print(f"part-1 answer: {solve([num[j] for j in mix(copy(num), list(range(len(num))))], num)}")
-print(f"part-2 answer: {solve(decrypted, num)}")
+print(f"part-2 answer: {solve([init[j] for j in idx], num)}")
