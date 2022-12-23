ef1974ca59e2d0be59fe5a5ad1919b8406bce357
alelouis <me@alelouis.eu>
2022-12-23 11:04:23 +0100

    day-23 2022

--- 2022/day-23/main.py
+++ 2022/day-23/main.py
@@ -15,10 +15,10 @@ while moving:
     old_elves, props = {e for e in elves}, {}
     for elf in elves:
         adj = {add(elf, v) for v in mov.values()}
-        if any(e in adj for e in elves):
+        if len(adj & elves) > 0:
             for i in range(4):
                 look = {add(elf, mov[d]) for d in di[cycles[(cycle+i)%4]]}
-                if all(e not in elves for e in look):
+                if len(look & elves)==0:
                     props[elf] = add(elf, mov[cycles[(cycle+i)%4]])
                     break
     rem_add = {(elf, prop) for elf, prop in props.items() if sum(prop==p for p in props.values()) == 1}
@@ -26,5 +26,4 @@ while moving:
         elves.remove(r)
         elves.add(a)
     cround, cycle, moving = cround + 1, cycle + 1, not (old_elves == elves)
-
 print(f'part-2: {cround}')
