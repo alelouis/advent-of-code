beebd82cdb3fe10983244100aef8333fbef90554
alelouis <me@alelouis.eu>
2024-12-03 11:37:40 +0100

    rfind has start and end, yes.

--- 2024/day-03/main.py
+++ 2024/day-03/main.py
@@ -2,6 +2,6 @@ from re import finditer
 from math import prod
 
 ops, r = open("input").read(), r'mul\((\d+),(\d+)\)'
-solve = lambda p: sum(prod(map(int, m.groups())) for m in finditer(r, ops) if p == 1 or ((ss := ops[:m.start()]).rfind("do()") >= ss.rfind("don't()")))
+solve = lambda p: sum(prod(map(int, m.groups())) for m in finditer(r, ops) if p == 1 or (ops.rfind("do()", 0, s := m.start()) >= ops.rfind("don't()", 0, s)))
 print(solve(1))
 print(solve(2))
