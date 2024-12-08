ebdb069e959576226f8cccab4b95be7d231d4a8f
alelouis <me@alelouis.eu>
2024-12-08 17:12:25 +0100

    day-08 2024

--- 2024/day-07/main.py
+++ 2024/day-07/main.py
@@ -3,7 +3,7 @@ lines = [(int(l.split(':')[0]), list(map(int, l.split(':')[1].strip().split())))
 
 def solve(t, n, part):
     ops = [lambda x, y: x + y, lambda x, y: x * y, lambda x, y: int(str(x) + str(y))]
-    return (t == n[0]) if len(n) == 1 else any([solve(*arg, part) for arg in [(t, [op(n[0], n[1])] + n[2:]) for op in ops[:part+1]]])
+    return t == n[0] if (len(n) == 1 or n[0] > t) else any([solve(*arg, part) for arg in [(t, [op(n[0], n[1])] + n[2:]) for op in ops[:part+1]]])
 
 
 print(list(sum(l[0] for l in lines if solve(*l, part=p)) for p in [1, 2]))
