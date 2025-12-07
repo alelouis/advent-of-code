a8de3cb258e081e3c1c996bd147cf865b45466b1
alelouis <me@alelouis.eu>
2025-12-07 10:42:07 +0100

    day-07 2025

--- 2025/day-07/main.py
+++ 2025/day-07/main.py
@@ -7,11 +7,10 @@ def split(active_beams):
         if beam in splitter:
             remove_beams.add(beam)
             new_beams |= {beam - 1j, beam + 1j}
-            timelines += counts[beam]
             counts[beam - 1j] += counts[beam]
             counts[beam + 1j] += counts[beam]
             counts[beam] = 0
-    return timelines, remove_beams, new_beams
+    return remove_beams, new_beams
 
 
 def down(old_beams):
@@ -25,12 +24,11 @@ map, counts = [l.strip() for l in open("input").readlines()], defaultdict(int)
 start, void, splitter = (set(r + 1j * c for r in range(len(map)) for c in range(len(map[0])) if map[r][c] == k) for k in "S.^")
 active_beams, counts[list(start)[0] + 1] = {list(start)[0] + 1}, 1
 
-splits, timelines = 0, 1
+splits = 0
 while list(active_beams)[0].real < len(map):
-    new_timelines, old_beams, new_beams = split(active_beams)
-    timelines += new_timelines
+    old_beams, new_beams = split(active_beams)
     splits += len(old_beams)
     active_beams = active_beams.difference(old_beams).union(new_beams)
     active_beams = down(active_beams)
 
-print(splits, timelines)
+print(splits, sum(counts[c] for c in counts if counts[c] > 0))
